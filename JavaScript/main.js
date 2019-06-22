27292a7f1161b718f7f22df98c980bc7e0b902e4
pachicodes <pachicarlson@gmail.com>
2019-06-22 15:45:54 -0400

    trying JS

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,3 +5,8 @@ var skies = document.querySelector("[value='skies']");
 pop = 7;
 light = 11;
 skies = 19;
+
+var result = function () {
+    document.querySelector("['quantity__input']") * document.querySelector("['choose__menu']");
+
+}
