4228dab5e142cec37622995ab00c1b3e26f76e25
pachicodes <pachicarlson@gmail.com>
2019-06-21 21:26:50 -0400

    creating variables

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,7 @@
 var pop = document.querySelector("[value='pop']");
 var light = document.querySelector("[value='light']");
 var skies = document.querySelector("[value='skies']");
+
+pop = 7;
+light = 11;
+skies = 19;
