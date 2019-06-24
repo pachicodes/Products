a5e122af3847e3d69fea797a082f126d2c6ee6a1
pachicodes <pachicarlson@gmail.com>
2019-06-24 07:44:13 -0400

    creating vars

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -6,4 +6,11 @@ pop = 7;
 light = 11;
 skies = 19;
 
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault()
+    var quantity = document.querySelector("[data-quantity]");
+    var choose = document.querySelector("[data-choose]");
+    
+}
 
+)
